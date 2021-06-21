e9627cbb8544c9f791c3435c303ad137390433fd
tjohnson-2 <85690659+tjohnson-2@users.noreply.github.com>
2021-06-21 10:21:10 -0400

    Update analyze_dev.py
    Removes extraneous functions and beautifies output.

--- analyze_dev.py
+++ analyze_dev.py
@@ -1,4 +1,3 @@
-#let's import some stuff
 import argparse
 import sys
 import statistics
@@ -7,7 +6,6 @@ import numpy
 
 parser = argparse.ArgumentParser(description='Analysis Options')
 
-#produce a log file from extracted data
 parser.add_argument('-l',
 	help='exports data from this script as a txt file',
 	action='store_true',
@@ -21,9 +19,12 @@ else:
 	outfile = sys.argv[2]
 
 fp=open(sys.argv[1])
+
 if args.l:
 	output = open(outfile, 'w')
+
 line=fp.readline()
+
 while line:
 	line=fp.readline()
 	words = line.split()
@@ -33,8 +34,7 @@ while line:
 numcol = len(words)
 header = words
 
-data = numpy.zeros((numcol,10000))
-print(data.shape,data)
+data = numpy.zeros((numcol,100000))
 
 ln = 0
 
@@ -44,45 +44,22 @@ while line:
 	if(words[0]=="Loop"):
 		break
 
-	print(words)
 	for i in range(numcol):
 		data[i][ln]=(float(words[i]))
 
+	ln_original = ln
 	ln = ln +1
 
-print("Number of lines of data:",ln)
-print("# header average stdev")
+print("\n")
+print("Number of steps:",ln_original)
+print(f"{'#' : <5} {'Header' : <6} {'Average' : ^24} {'StDev' : ^6}")
 for i in range(numcol):
-	print(i,header[i],numpy.average(data[i,:ln]),numpy.std(data[i,:ln]))
+	print(f"{i : <5} {header[i] :<8} {(numpy.average(data[i,:ln])) :>15.5f} {(numpy.std(data[i,:ln])) :>15.5f}",)
+print("\n")
 
 exit()
 
-#Mean output file writing
-if args.ta and args.l:
-	output.write('Temperature standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(mean))
-if args.pa and args.l:
-	output.write('Pressure standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(mean))
-if args.va and args.l:
-	output.write('Volume standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(mean))
-if args.sa and args.l:
-	output.write('Surface tension standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(mean))
-
-
-#SD output file writing
-if args.tdev and args.l:
-	output.write('Temperature standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(deviation))
-if args.pdev and args.l:
-	output.write('Pressure standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(deviation))
-if args.vdev and args.l:
-	output.write('Volume standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(deviation))
-if args.sdev and args.l:
-	output.write('Surface tension standard deviation of ' + sys.argv[1]  + ' is' + ' ' + str(deviation))
-
-
 fp.close()
+
 if args.l:
 	output.close()
-if args.t or args.p or args.v or args.s:
-	print(ypoints)
-	plt.plot(ypoints)
-	plt.show()
